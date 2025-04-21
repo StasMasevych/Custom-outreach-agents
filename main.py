b3059d7687aebf9e21e7261facd2d48e21ab03f6
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2025-04-21 12:17:13 +0200

    add custom tool and instructions

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from crewai import Agent, Task, Crew
-
+# Standard libraries
 import os
+
+# Third-party libraries
+from crewai import Agent, Task, Crew
+from crewai_tools import DirectoryReadTool, FileReadTool, SerperDevTool
+
+# Third-party libraries - custom tool
+from crewai_tools import BaseTool
+
+# Local project utilities
 from utils import get_openai_api_key, pretty_print_result
 from utils import get_serper_api_key
-
-from crewai_tools import DirectoryReadTool, FileReadTool, SerperDevTool
 
 openai_api_key = get_openai_api_key()
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-4-turbo'
@@ -60,3 +66,104 @@ lead_sales_rep_agent = Agent(
 directory_read_tool = DirectoryReadTool(directory='./instructions')
 file_read_tool = FileReadTool()
 search_tool = SerperDevTool()
+
+
+# Custom tools
+class SentimentAnalysisTool(BaseTool):
+    name: str ="Sentiment Analysis Tool"
+    description: str = ("Analyzes the sentiment of text "
+         "to ensure positive and engaging communication.")
+    
+    def _run(self, text: str) -> str:
+        # Your custom code tool goes here
+        return "positive"
+
+sentiment_analysis_tool = SentimentAnalysisTool()
+
+
+# Create tasks
+
+lead_profiling_task = Task(
+    description=(
+        "Conduct an in-depth analysis of {lead_name}, "
+        "a company in the {industry} sector "
+        "that recently showed interest in our solutions. "
+        "Utilize all available data sources "
+        "to compile a detailed profile, "
+        "focusing on key decision-makers, recent business "
+        "tell about decision-makers taking data from LinkedIn and other sources"
+        "developments, and potential needs "
+        "that align with our offerings. "
+        "This task is crucial for tailoring "
+        "our engagement strategy effectively.\n"
+        "Don't make assumptions and "
+        "only use information you absolutely sure about."
+    ),
+    expected_output=(
+        "A comprehensive report on {lead_name}, "
+        "including company background, "
+        "key personnel, recent milestones, and identified needs. "
+        "Highlight potential areas where "
+        "our solutions can provide value, "
+        "and suggest personalized engagement strategies."
+    ),
+    tools=[directory_read_tool, file_read_tool, search_tool],
+    agent=sales_rep_agent,
+)
+
+personalized_outreach_task = Task(
+    description=(
+        "Using the insights gathered from "
+        "the lead profiling report on {lead_name}, "
+        "craft a personalized outreach campaign "
+        "aimed at {key_decision_maker}, "
+        "the {position} of {lead_name}. "
+        "The campaign should address their recent {milestone} "
+        "and how our solutions can support their goals. "
+        "Your communication must resonate "
+        "with {lead_name}'s company culture and values, "
+        "demonstrating a deep understanding of "
+        "their business and needs.\n"
+        "Don't make assumptions and only "
+        "use information you absolutely sure about."
+    ),
+    expected_output=(
+        "A series of personalized email drafts "
+        "tailored to {lead_name}, "
+        "specifically targeting {key_decision_maker}."
+        "Each draft should include "
+        "a compelling narrative that connects our solutions "
+        "with their recent achievements and future goals. "
+        "Ensure the tone is engaging, professional, "
+        "and aligned with {lead_name}'s corporate identity."
+    ),
+    tools=[sentiment_analysis_tool, search_tool],
+    agent=lead_sales_rep_agent,
+)
+
+# Create crew
+
+crew = Crew(
+    agents=[sales_rep_agent, 
+            lead_sales_rep_agent],
+    
+    tasks=[lead_profiling_task, 
+           personalized_outreach_task],
+	
+    verbose=2,
+	memory=True
+)
+
+# Run crew
+
+inputs = {
+    "lead_name": "Voltage Group",
+    "industry": "Renewable Energy Engineering",
+    "key_decision_maker": "Dmytro Nechypurenko",
+    "position": "CBDO",
+    "milestone": "recent BESS projects development in Poland"
+}
+
+result = crew.kickoff(inputs=inputs)
+
+print(pretty_print_result(result))
